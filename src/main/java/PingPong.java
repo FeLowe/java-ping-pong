65c67686574ce92b362e71f0602592238667df86
Fernanda Lowe and John Klein <john@>
2016-04-18 13:48:23 -0700

    fulfills all four specs

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,10 +1,30 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class PingPong {
   public static void main(String[] args) {}
+
+
+  public ArrayList<String> outputPingPong(Integer input) {
+    ArrayList<String> numbersArray = new ArrayList<String>();
+    for ( Integer i = 1; i <= 15; i++) {
+      if ( i % 15 == 0 ) {
+        numbersArray.add("Ping-Pong");
+      } else if ( i % 5 == 0 ) {
+        numbersArray.add("Pong");
+      } else if ( i % 3 == 0 ) {
+        numbersArray.add("Ping");
+      } else {
+        numbersArray.add(Integer.toString(i));
+      }
+    }
+
+
+  return numbersArray;
+  }
 
 }
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -1,12 +1,29 @@
+import java.util.ArrayList;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class PingPongTest {
 
   @Test
-   public void isLeapYear_forNumberDivisibleByFour_true() {
-     LeapYear leapYear = new LeapYear();
-     assertEquals(true, leapYear.isLeapYear(2012));
-    }
+  public void isPingPong_forNumberNotDivisibleByThreeFiveAndFifthteen_number() {
+    PingPong pingPong = new PingPong();
+    ArrayList<String> expected = new ArrayList<String>();
+    expected.add("1");
+    expected.add("2");
+    expected.add("Ping");
+    expected.add("4");
+    expected.add("Pong");
+    expected.add("Ping");
+    expected.add("7");
+    expected.add("8");
+    expected.add("Ping");
+    expected.add("Pong");
+    expected.add("11");
+    expected.add("Ping");
+    expected.add("13");
+    expected.add("14");
+    expected.add("Ping-Pong");
+    assertEquals(expected, pingPong.outputPingPong(2));
+  }
 
 }
