b857a16da4e6aed9339c67f32c20197d66b03073
Fernanda Lowe and John Klein <john@>
2016-04-18 14:42:43 -0700

    added working ui

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -6,12 +6,35 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class PingPong {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
+
+      String number = request.queryParams("number");
+      Integer integerNumber = Integer.parseInt(number);
+      ArrayList<String> pingPongOutput = outputPingPong(integerNumber);
+
+      model.put("pingPongOutput", pingPongOutput);
+      model.put("number", request.queryParams("number"));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  }
 
 
-  public ArrayList<String> outputPingPong(Integer input) {
+  public static ArrayList<String> outputPingPong(Integer input) {
     ArrayList<String> numbersArray = new ArrayList<String>();
-    for ( Integer i = 1; i <= 15; i++) {
+    for ( Integer i = 1; i <= input; i++) {
       if ( i % 15 == 0 ) {
         numbersArray.add("Ping-Pong");
       } else if ( i % 5 == 0 ) {
